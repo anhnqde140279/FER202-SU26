0d98d81f4cf4a33f388e2ea705b019bd76aba37d
QuynhAnh4100 <mka251099@gmail.com>
2026-05-18 14:42:08 +0700

    Add cau 4

---
+++ Code/slot3/ex1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+import ListPerson from './components/ListPerson';
+
+function App() {
+  return (
+    <div>
+     <Hello />
+     <ListPerson />
+    </div>
+  );
+}
+
+export default App;
