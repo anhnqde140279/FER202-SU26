196857ea687026bd89fdaf5238498ca0c8b08db4
QuynhAnh4100 <mka251099@gmail.com>
2026-05-27 16:09:19 +0700

    nop bai slot6

---
+++ slot6/ex2/react-blog-app/src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';  // <- thêm dòng này
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
